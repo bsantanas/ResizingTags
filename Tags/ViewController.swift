3ec259ab6ba3f7d9199b7b3fb5e4fe5697bacfcb
bsantanas <bsantana@hellobundle.com>
2015-06-23 11:13:30 +0100

    Initial Commit

---
+++ Tags/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tags
+//
+//  Created by Bernardo Santana on 6/23/15.
+//  Copyright (c) 2015 bundle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
